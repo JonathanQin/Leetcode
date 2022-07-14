ac7815c1c6e7d1900e8ae8e3ab63a9d6a55cb240
Jonathan Qin <jonathan.qin@gmail.com>
2022-07-13 23:59:42 -0700

    in progress

---
+++ 95-Unique-Binary-Search-Trees-II.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int n) {
+
+    }
+    void createTree(TreeNode *parent, int range)
+};
+
+int main(){
+    Solution solution;
+    solution.generateTrees(3);
+    return 0;
+}
