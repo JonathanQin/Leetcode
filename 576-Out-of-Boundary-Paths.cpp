267eed395ed41a9973f0a79c6c58dfed4f8af6fa
Jonathan Qin <jonathan.qin@gmail.com>
2022-07-16 00:48:28 -0700

    includes both dp and recursive solution

--- 576-Out-of-Boundary-Paths.cpp
+++ 576-Out-of-Boundary-Paths.cpp
@@ -5,20 +5,59 @@ using namespace std;
 class Solution {
 public:
     int MOD = 1000000007;
-    long long memory[51][51][51];
+    int dp[51][51][51];
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        int res = 0;
-        for(int i = 0; i < 51; i++){
-            for(int j = 0; j < 51; j++){
-                for(int k = 0; k < 51; k++){
+        long long memory[51][51][51];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                for(int k = 0; k <= maxMove; k++){
                     memory[i][j][k] = -1;
+                    dp[i][j][k] = 0;
                 }
             }
         }
-        return recurse(m, n, maxMove, startRow, startColumn, 0);
+        int recursive_solution = recurse(m, n, maxMove, startRow, startColumn, 0, memory);
+        int dp_solution = dpSolution(m, n, maxMove, startRow, startColumn);
+        return dp_solution;
     }
 
-    long long recurse(int m, int n, int maxMove, int row, int col, int cnt){
+
+    int dpSolution(int m, int n, int maxMove, int row, int col){
+        int res = 0;
+        dp[row][col][0] = 1;
+        for(int k = 1; k <= maxMove; k++){
+            for(int i = 0; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    if(i != 0){
+                        dp[i][j][k] = (dp[i][j][k]+dp[i-1][j][k-1])%MOD;
+                    }
+                    if(i != m-1){
+                        dp[i][j][k] = (dp[i][j][k]+dp[i+1][j][k-1])%MOD;
+                    }
+                    if(j != 0){
+                        dp[i][j][k] = (dp[i][j][k]+dp[i][j-1][k-1])%MOD;
+                    }
+                    if(j != n-1){
+                        dp[i][j][k] = (dp[i][j][k]+dp[i][j+1][k-1])%MOD;
+                    }
+                }
+            }
+        }
+        for(int k = 0; k < maxMove; k++){
+            for(int i = 0; i < m; i++){
+                res = (res+dp[i][0][k])%MOD;
+                res = (res+dp[i][n-1][k])%MOD;
+            }
+            for(int j = 0; j < n; j++){
+                res = (res+dp[0][j][k])%MOD;
+                res = (res+dp[m-1][j][k])%MOD;
+            }
+        }
+        return res;
+    }
+
+    template<int R, int C, int move>
+    long long recurse(int m, int n, int maxMove, int row, int col, int cnt, long long(&memory)[R][C][move]){
         if(cnt > maxMove){
             return 0;
         }
@@ -29,7 +68,7 @@ public:
         {
             return memory[row][col][maxMove-cnt];
         }
-        memory[row][col][maxMove-cnt] = (recurse(m, n, maxMove, row-1, col, cnt+1) + recurse(m, n, maxMove, row+1, col, cnt+1) + recurse(m, n, maxMove, row, col+1, cnt+1) + recurse(m, n, maxMove, row, col-1, cnt+1))%MOD;
+        memory[row][col][maxMove-cnt] = (recurse(m, n, maxMove, row-1, col, cnt+1, memory) + recurse(m, n, maxMove, row+1, col, cnt+1, memory) + recurse(m, n, maxMove, row, col+1, cnt+1, memory) + recurse(m, n, maxMove, row, col-1, cnt+1, memory))%MOD;
         return memory[row][col][maxMove-cnt]; 
     }
 };
