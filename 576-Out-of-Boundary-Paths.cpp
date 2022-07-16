ddec60e4bf1d79a019bd765aeb3869831449f52b
Jonathan Qin <jonathan.qin@gmail.com>
2022-07-16 00:15:57 -0700

    recursive memory solution

--- 576-Out-of-Boundary-Paths.cpp
+++ 576-Out-of-Boundary-Paths.cpp
@@ -5,21 +5,20 @@ using namespace std;
 class Solution {
 public:
     int MOD = 1000000007;
+    long long memory[51][51][51];
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
         int res = 0;
-        size_t memory[51][51][51];
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                for(int k = 0; k <= maxMove; k++){
+        for(int i = 0; i < 51; i++){
+            for(int j = 0; j < 51; j++){
+                for(int k = 0; k < 51; k++){
                     memory[i][j][k] = -1;
                 }
             }
         }
-        return recurse(m, n, maxMove, startRow, startColumn, 0, memory);
+        return recurse(m, n, maxMove, startRow, startColumn, 0);
     }
 
-    template<int R, int C, int M>
-    size_t recurse(int m, int n, int maxMove, int row, int col, int cnt, size_t(&memory)[R][C][M]){
+    long long recurse(int m, int n, int maxMove, int row, int col, int cnt){
         if(cnt > maxMove){
             return 0;
         }
@@ -30,7 +29,7 @@ public:
         {
             return memory[row][col][maxMove-cnt];
         }
-        memory[row][col][maxMove-cnt] = (recurse(m, n, maxMove, row-1, col, cnt+1, memory) + recurse(m, n, maxMove, row+1, col, cnt+1, memory) + recurse(m, n, maxMove, row, col+1, cnt+1, memory) + recurse(m, n, maxMove, row, col-1, cnt+1, memory))%MOD;
+        memory[row][col][maxMove-cnt] = (recurse(m, n, maxMove, row-1, col, cnt+1) + recurse(m, n, maxMove, row+1, col, cnt+1) + recurse(m, n, maxMove, row, col+1, cnt+1) + recurse(m, n, maxMove, row, col-1, cnt+1))%MOD;
         return memory[row][col][maxMove-cnt]; 
     }
 };
